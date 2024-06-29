4db264ac3cf52fcfa520ae22e98c1ec1793c17ab
afiovinicius <afiovinicius@gmail.com>
2024-06-28 21:37:29 -0300

    fix: upload das secrets key

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,8 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
+from app.api.libs.redis import check_redis_connection
+from app.core.database import engine
 from app.main import app
 
 client = TestClient(app)
@@ -11,3 +13,13 @@ def test_root():
     response = client.get("/")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Hello World"}
+
+
+def test_check_redis_connection():
+    connection_status = check_redis_connection()
+    assert connection_status is not None
+
+
+def test_check_database_connection():
+    with engine.connect() as connection:
+        assert connection is not None
