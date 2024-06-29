b42aa598ee0c9e8b002e2706bbd194efd8bd63cd
afiovinicius <afiovinicius@gmail.com>
2024-06-28 21:24:23 -0300

    feat: adicionando teste para garantir conexão com redis e banco de dados

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,6 @@
 from http import HTTPStatus
 
-import pytest
 from fastapi.testclient import TestClient
-from httpx import AsyncClient
 
 from app.api.libs.redis import check_redis_connection
 from app.core.database import engine
@@ -11,29 +9,17 @@ from app.main import app
 client = TestClient(app)
 
 
-@pytest.fixture()
-async def async_client():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        yield ac
-
-
-@pytest.mark.asyncio()
-async def test_root(async_client):
-    response = await async_client.get("/")
+def test_root():
+    response = client.get("/")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Hello World"}
 
 
-@pytest.mark.asyncio()
-async def test_check_redis_connection():
-    connection_status = await check_redis_connection()
-    assert connection_status is True
+def test_check_redis_connection():
+    connection_status = check_redis_connection()
+    assert connection_status is not None
 
 
-@pytest.mark.asyncio()
-async def test_check_database_connection():
-    try:
-        with engine.connect() as connection:
-            assert connection is not None
-    except Exception as e:
-        pytest.fail(f"Falha ao conectar ao banco de dados: {e}")
+def test_check_database_connection():
+    with engine.connect() as connection:
+        assert connection is not None
