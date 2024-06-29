7d92499ef92075a78cac5fcf50e4fb10278ccf5e
afiovinicius <afiovinicius@gmail.com>
2024-06-28 21:30:39 -0300

    feat: teste de rota

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,8 +2,6 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
-from app.api.libs.redis import check_redis_connection
-from app.core.database import engine
 from app.main import app
 
 client = TestClient(app)
@@ -13,13 +11,3 @@ def test_root():
     response = client.get("/")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Hello World"}
-
-
-def test_check_redis_connection():
-    connection_status = check_redis_connection()
-    assert connection_status is not None
-
-
-def test_check_database_connection():
-    with engine.connect() as connection:
-        assert connection is not None
